e8dc0257ce70eb85b529ea69d588facea6bd839a
Li-Chun Chung <li-chun_chung@Leo-Chung.local>
2021-09-21 15:12:37 -0700

    hw9-21

---
+++ pythonBasics.py
@@ -0,0 +1,31 @@
+def main():
+
+    #create a module and add the main method
+
+    #the name of the assignment and the date
+    print ("Python Basic Commands Practice")
+    print ("9/21/2021")
+
+    
+    #the sum and product of the two variables 
+    var1= 40
+    var2= 67
+
+    print (str(var1+var2))
+
+    print (var1*var2)
+
+    
+    #number of students 
+    stunum1= 20
+    stunum2= 80
+
+    print ("There are currently "+str(stunum1)+" number of students in the USB.")
+    print ("There are currently "+str(stunum2)+" number of students in the USB.")
+    print ("There are currently "+str(114)+" number of students in the USB.")
+
+    
+    
+
+if __name__ == "__main__":
+    main()  
