14b2f94ea52ade53a2b10b063906aa03daa8e3f0
Li-Chun Chung <li-chun_chung@Leo-Chung-2.local>
2021-10-05 14:37:57 -0700

    PythonLabOne.SecondVersion

--- pythonLabOne.py
+++ pythonLabOne.py
@@ -3,7 +3,7 @@ def coffeeShop():
     receipt=""
     name= input("Welcome! What is your name?\n")
     order= input("What would you like to order?\n")
-    receipt = (name +" "+order+ " "+str(10))
+    receipt = (name +" order "+order+ ". their order number is "+str(10)+".")
     return receipt
 
     
@@ -12,12 +12,14 @@ def palindrome(s):
     for i in range (len(s)):
         if s[i] == s[len(s)-i-1:len(s)-i]:
             pali = True
+        else:
+            pali = False
 
     return pali
 
 def main():
     print(coffeeShop())
-    x=input("enter something")
+    x=input("enter something\n")
     print(palindrome(x))
 
 
