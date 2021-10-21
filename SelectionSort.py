15ee9120ee96b6172d463dc37e18ea39c73ea13b
Li-Chun Chung <li-chun_chung@Leo-Chung-2.local>
2021-10-21 15:03:45 -0700

    SelectionSort

---
+++ SelectionSort.py
@@ -0,0 +1,28 @@
+def sortNum(x):
+    Min=0
+    num=0
+
+    for i in range(len(x)):
+        Min=x[i]
+        for j in range (i,len(x)):
+            if (x[j]<Min):
+                Min= x[j]
+                num=j
+
+        if (Min != x[i]):
+            x[num]=x[i]
+            x[i]=Min
+
+    return x
+        
+                
+    
+
+def main():
+    a=[2,5,3,21,1,100,25,40, 60, 9, 29, -2, 0, -10000000, 100000000]
+
+    print(sortNum(a))
+    
+
+if __name__ == "__main__":
+    main()
